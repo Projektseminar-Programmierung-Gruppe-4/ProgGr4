b2c6ee903135cd19fa2d4626319387c088e9a0d9
Tim Konle <tim.konle@web.de>
2021-11-10 20:13:36 +0100

    posts anlegen und einsehen fertig

--- Gr4/Forum/views.py
+++ Gr4/Forum/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Forum.models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
 
 # Create your views here.
 
@@ -10,5 +12,16 @@ def overview(request):
     return render(request, 'Forum/index.html', {'posts': posts})
 
 def create_post(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.voteCount = 0
+            post.save()
+            return redirect('overview')
+    else:
+        form = PostForm()
     return render(request, 'Forum/create.html', {'form': form})
+
