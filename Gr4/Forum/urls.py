be3bb7f9fcdb18c04877b0a17057b0900d1e7534
Tim Konle <tim.konle@web.de>
2021-11-10 16:16:28 +0100

    Add functions and templates

--- Gr4/Forum/urls.py
+++ Gr4/Forum/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.overview, name = 'overview'),
+    path('create', views.create_post, name = 'create_post')
+]
--- Gr4/Forum/views.py
+++ Gr4/Forum/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Forum.models import Post
+from .forms import PostForm
 
 # Create your views here.
 
+def overview(request):
+    posts = Post.objects.all()
+    return render(request, 'Forum/index.html', {'posts': posts})
+
+def create_post(request):
+    form = PostForm()
+    return render(request, 'Forum/create.html', {'form': form})
