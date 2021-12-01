bb88d6766b1716be4f98b600f146167d0f2bf7b9
Munzeii <55600871+Munzeii@users.noreply.github.com>
2021-12-01 14:35:30 +0100

    Update tests.py

--- Gr4/Forum/tests.py
+++ Gr4/Forum/tests.py
@@ -1,3 +1,10 @@
+from django.http import response
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class URLTests(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
