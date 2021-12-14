8889f34ea919ebcdf19614fa1ec6cf7900aa3971
Munzeii <55600871+Munzeii@users.noreply.github.com>
2021-12-14 11:51:48 +0100

    Update tests.py

--- Gr4/Forum/tests/tests.py
+++ Gr4/Forum/tests/tests.py
@@ -1,3 +1,6 @@
+from typing import Text
+from django.contrib.auth.signals import user_logged_in
+from django.forms.fields import EmailField
 from django.http import response
 from django.test import TestCase
 #from Forum.models import Forum
@@ -5,7 +8,8 @@ from django.utils import timezone
 from django.urls import reverse
 #from Forum.forms import Forum
 # Create your tests here.
-
+from django.contrib.auth import get_user_model
+from Forum.models import Post
 
 class URLTests(TestCase):
     def test_homepage(self):
@@ -16,10 +20,34 @@ class URLTests(TestCase):
 
 class VIEWTests(TestCase):
         
-        def test_Forum_view(self):
-            w = self.create_whatever()
-            url = reverse("Forum.views")
-            resp = self.client.get(url)
+    def test_Forum_view(self):
+        
+        url = reverse("Forum.urls")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        
 
-            self.assertEqual(resp.status_code, 200)
-            self.assertIn(w.title, resp.content)
+
+User = get_user_model
+class UserTest(TestCase):
+    def setUp(self):
+        user_a = User(username ='Tim',email='test@gmx.de')
+        user_a.is_staff = True
+        user_a.is_superuser = True
+        user_a.set_password('some_234_wwf')
+        user_a.save()
+        print(user_a.id)
+
+    def test_user_exists(self):
+        user_count = User.objects.all().count
+        self.assertEqual(user_count,1) 
+
+
+
+class TestForumModel(TestCase):
+
+    def test_model(self):
+        title = Post.objects.create(title="Test Title")
+        text = Post.objects.create(text="Test text ")
+
+        self.assertEqual(str(title), "Test Title")
