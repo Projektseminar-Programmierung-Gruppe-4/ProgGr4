3a480796c015f28433973ed790298d87ef32a81b
Tim Konle <tim.konle@web.de>
2022-01-09 22:50:36 +0100

    commenting + last changes

--- Gr4/Forum/models.py
+++ Gr4/Forum/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-#Klasse Post erstellt neue Tabelle in der Datenbank für die Forumbeiträge
+#class for posts in the Forum with fk = user_id
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     #categorie = models.ForeignKey()
@@ -27,6 +27,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+#class for comments with fk = post_id,user_id
 class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name="comments", blank=True, null=True)
@@ -38,6 +39,7 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
+#class for subcomments with fk = comment_id,user_id
 class Subcomment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     comment = models.ForeignKey(Comment, on_delete = models.CASCADE, related_name="subcomments", blank=True, null=True)
@@ -49,6 +51,7 @@ class Subcomment(models.Model):
     def __str__(self):
         return self.text
 
+#class for storing the status of the votes for Comments depending on the user
 class Votes(models.Model):
     comment = models.ForeignKey(Comment,on_delete = models.CASCADE, related_name="vote", blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
@@ -59,6 +62,7 @@ class Votes(models.Model):
     def __str__(self):
         return self.id
 
+#class for storing the status of the votes for Posts depending on the user
 class Postvotes(models.Model):
     post = models.ForeignKey(Post,on_delete = models.CASCADE, related_name="postvote", blank=True , null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE , blank=True, null=True)
@@ -69,6 +73,7 @@ class Postvotes(models.Model):
     def __str__(self):
         return self.id
 
+#class for storing the status of the votes for subcomments depending on the user
 class Subcommentvotes(models.Model):
     subcomment = models.ForeignKey(Subcomment,on_delete = models.CASCADE, related_name="subcommentvote" , blank=True , null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE , blank=True, null=True)
@@ -80,13 +85,14 @@ class Subcommentvotes(models.Model):
         return self.id
 
 
-
+#class for the departments, user can be assigned to
 class Department(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+#Class for the fields which are not to be found in the user model (birthdate and department)
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -96,7 +102,7 @@ class Employee(models.Model):
     def __str__(self):
         return self.user.username
 
-#muss noch migriert werden
+#class to store reported posts(fk=user_id,post_id)
 class Postreport(models.Model):
     post = models.ForeignKey(Post,on_delete = models.CASCADE, related_name="post")
     reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -106,6 +112,7 @@ class Postreport(models.Model):
     def __str__(self):
         return self.post.title
 
+#class to store reported comments (fk=user_id,comment_id)
 class Commentreport(models.Model):
     comment = models.ForeignKey(Comment,on_delete = models.CASCADE, related_name="comment")
     reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -115,6 +122,7 @@ class Commentreport(models.Model):
     def __str__(self):
         return self.comment.text
 
+#class to store reported comments (fk=user_id,subcomment_id)
 class Subcommentreport(models.Model):
     subcomment = models.ForeignKey(Subcomment,on_delete = models.CASCADE, related_name="subcomment")
     reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- Gr4/Forum/views.py
+++ Gr4/Forum/views.py
@@ -26,16 +26,22 @@ from django.views.generic import ListView
 from django.db.models import Q
 from django.urls import resolve
 
-# Create your views here.
+# Here are the functions, which fills the templates with life
+# Our views is function based structured
 
-#eventuelle Filterung hier einbauen
+
+##############################function for view sites, eg. overview, detail, commentdetail etc##############
+#function for base.html, records are sorted depending on filter parameters 
 def overview(request):
     posts = Post.objects.all()
     filter = request.GET.get('choicefilter')
     search = request.GET.get('search')
 
+    #check if search term is in filter 
     if search != '' and search is not None:
         posts = posts.filter(Q(title__icontains = search) | Q(text__icontains = search))
+
+    #check if a choice in the filter is applied
     if filter != '' and filter is not None:
         if filter == 'votecount-desc':
             posts = posts.order_by('-voteCount')
@@ -48,20 +54,20 @@ def overview(request):
 
     return render(request, 'Forum/base.html', {'posts': posts})
 
-#Funktion für Archivseite mit allen Posts und Kommentaren des Users
+#Function for archive page with all posts,comments and subcomments of the user
 def archiv(request):
     posts = Post.objects.filter(author = request.user)
     comments = Comment.objects.filter(author = request.user)
     subcomments = Subcomment.objects.filter(author = request.user)
     return render(request, 'Forum/archiv.html', {'posts': posts, 'comments': comments,'subcomments': subcomments})
 
-#Funktion zum anzeigen der Adminseite
+#function to show adminsite, only available with is_staff status 
 def adminpage(request):
     if request.user.is_staff == False:
         return HttpResponse('Unauthorized', status=401)
     return render (request, 'Forum/adminpage.html')
 
-#Funktion zum anzeigen der gemeldeten Posts,Kommentare und Antworten
+#function to show all reported posts, comments and subcomments, only available with is_staff
 def reports(request):
     if request.user.is_staff == False:
         return HttpResponse('Unauthorized', status=401)
@@ -71,22 +77,24 @@ def reports(request):
     reported_subcomments = Subcommentreport.objects.all()
     return render(request, 'Forum/reports.html', {'posts': reported_posts, 'comments': reported_comments, 'subcomments': reported_subcomments})
 
+#function for permission overview site
 def permission_overview(request):
     if request.user.is_staff == False:
         return HttpResponse('Unauthorized', status=401)
     users = User.objects.all().order_by('-is_staff')
     return render(request, 'Forum/permissions.html', {'users': users})
 
-#Views für eine Detailseite, welche einen Post mit seinen dazugehörigen Kommentaren anzeigt 
-#und Kommentare hinzufügen lässt
-#Eingangsparameter Anfrage und pk des posts
+#Views for a detail page, which displays a post with its associated comments. 
+#and lets you add comments
+#input parameters request and pk of the post
 def detail(request, pk):
-    #get post mit pk = request pk
+    #get post with pk = request pk
     post = get_object_or_404(Post, pk=pk)
     url = "/post/" + pk
-    #get alle comments mit fk = post-pk
+    #get all comments with fk = post-pk
     comments = post.comments.filter(post_id = pk).order_by('-voteCount')
-    #speichern eines Kommentars, wenn Kommentarfeld genutzt wird (request.method = Post)
+    
+    #save a comment, if commentfield is used(request.method = Post)
     if request.method == "POST":
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -96,11 +104,37 @@ def detail(request, pk):
             comment.post_id = pk
             comment.save()
             return redirect(url)
-    #sonst CommentForm() nur anzeigens
+    #wenn method is not post, show emtpy Form
     else:
         comment_form = CommentForm()
     return render(request, 'Forum/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
 
+#function for comment detail page, input param pk
+#page shows: post + comment + subcomment for this comment
+def detail_comment(request, pk):
+    #get comments, posts and subcomments with comment_pk
+    comment = Comment.objects.get(pk=pk)
+    post = Post.objects.get(pk=comment.post_id)
+    subcomments = Subcomment.objects.filter(comment_id = pk).order_by('-voteCount')
+    url = "/comment/" + str(comment.id) + "/detail/"
+    #save the subcomment if form is used
+    if request.method == "POST":
+        subcomment_form = SubcommentForm(request.POST)
+        if subcomment_form.is_valid():
+            subcomment = subcomment_form.save(commit=False)
+            subcomment.author = request.user
+            subcomment.voteCount = 0
+            subcomment.comment_id = pk
+            subcomment.save()
+            return redirect(url)
+    #else only show empty Form
+    else:
+        subcomment_form = SubcommentForm()
+    return render(request, 'Forum/commentDetail.html', {'post': post, 'comment': comment,'subcomments': subcomments, 'form': subcomment_form})
+
+
+#########################functions to create, update, delete and report posts##########################
+#create post over PostForm
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -116,12 +150,15 @@ def create_post(request):
         form = PostForm()
     return render(request, 'Forum/create.html', {'form': form})
 
+#function for reporting a post, user can report a post with a report commment
 def report_post(request, pk):
+    #get post over pk
     entry = Post.objects.get(pk=pk)
     url = '/post/' + str(pk)
     if request.method == "POST":
         form = ReportPostForm(request.POST)
         if form.is_valid():
+            #save post with comment to Postreport Modell
             report = form.save(commit=False)
             report.reporter = request.user
             report.report_date = timezone.now()
@@ -134,6 +171,7 @@ def report_post(request, pk):
     form = ReportPostForm()
     return render(request, 'Forum/reportPost.html', {'form': form, 'entry': entry})
 
+#function for updating post: title+text
 def update_post(request, pk):
     post = Post.objects.get(pk=pk)
     url = '/post/' + str(post.id)
@@ -146,14 +184,22 @@ def update_post(request, pk):
     
     return render(request, 'Forum/updatePost.html', {'form': form})
 
+#delete post via pk
 def delete_post(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
     return redirect('overview')
 
+
+#voting function to vote post, comment or subcomment
+#entry parameters are pk of the entry, vote(like or dislike) and type of entry(post,comment or subcomment)
 def vote(request, pk, vote, typ):
     reaction = vote
+    #get url from page from which the request came
     url = request.META.get('HTTP_REFERER')
+
+    #if-else with parameter typ to get the right models to update or create
+    #(post-postvotes, comment-commentvotes or subcomment-subcommentvotes)
     if typ == 'post':
         entry = Post.objects.get(pk = pk)
 
@@ -190,16 +236,16 @@ def vote(request, pk, vote, typ):
         vote = Subcommentvotes.objects.get(subcomment_id = pk, author_id = request.user)
 
 
-    #überprüfen ob like Button, den Request ausgelöst hat
+    #check if request is from like button
     if reaction == 'like':
-        #überprüfen ob Like-Button schon aktiv ist
+        #check if like-button is already active
         if vote.like == True:
             vote.like = False
             entry.voteCount -= 1
             vote.save()
             entry.save()
             return redirect(url)
-        #überprüfen ob dislike button aktiv ist
+        #check if dislike-button is already active
         elif vote.dislike == True:
             vote.dislike = False
             vote.like = True
@@ -207,15 +253,16 @@ def vote(request, pk, vote, typ):
             vote.save()
             entry.save()
             return redirect(url)
-        #falls noch kein Button aktiv ist
+        #if no button is active
         else:
             vote.like = True
             entry.voteCount += 1
             vote.save()
             entry.save()
             return redirect(url)
-    
+    #check if request is from like button
     if reaction == 'dislike':
+        #check if dislike-button is already active
         if vote.dislike == True:
             vote.dislike = False
             entry.voteCount += 1
@@ -223,6 +270,7 @@ def vote(request, pk, vote, typ):
             entry.save()
             return redirect(url)
 
+        #check if like-button is already active
         elif vote.like == True:
             vote.like = False
             vote.dislike = True
@@ -231,6 +279,7 @@ def vote(request, pk, vote, typ):
             entry.save()
             return redirect(url)
 
+        #if no button is active
         else:
             vote.dislike = True
             entry.voteCount -= 1
@@ -242,7 +291,9 @@ def vote(request, pk, vote, typ):
     return redirect(url)
 
 
-# Funktionen für Kommentare (update,delete,vote) --> müssen noch geschrieben werden
+#################### functions for comments (update,delete,report, detail page)#########################
+
+#update comment text
 def update_comment(request, pk):
     comment = Comment.objects.get(pk=pk)
     post = comment.post_id
@@ -255,6 +306,7 @@ def update_comment(request, pk):
     
     return render(request, 'Forum/updateComment.html', {'form': form})
 
+#delete comment
 def delete_comment(request, pk):
     comment = Comment.objects.get(pk=pk)
     post = comment.post_id
@@ -263,13 +315,16 @@ def delete_comment(request, pk):
     comment.delete()
     return redirect(url)
 
+#report comment via pk
 def report_comment(request, pk):
+    #get comment via pk
     entry = Comment.objects.get(pk=pk)
     post = entry.post_id
     url = '/post/' + str(post)
     if request.method == "POST":
         form = ReportCommentForm(request.POST)
         if form.is_valid():
+            #save comment with report comment into ReportComment Model
             report = form.save(commit=False)
             report.reporter = request.user
             report.report_date = timezone.now()
@@ -281,28 +336,9 @@ def report_comment(request, pk):
         form = ReportCommentForm()
     return render(request, 'Forum/reportPost.html', {'form': form, 'entry': entry})
 
-#Funktionen für subcomments
-def detail_comment(request, pk):
-    #get alle comments mit fk = post-pk
-    comment = Comment.objects.get(pk=pk)
-    post = Post.objects.get(pk=comment.post_id)
-    subcomments = Subcomment.objects.filter(comment_id = pk).order_by('-voteCount')
-    url = "/comment/" + str(comment.id) + "/detail/"
-    #speichern eines Kommentars, wenn Kommentarfeld genutzt wird (request.method = Post)
-    if request.method == "POST":
-        subcomment_form = SubcommentForm(request.POST)
-        if subcomment_form.is_valid():
-            subcomment = subcomment_form.save(commit=False)
-            subcomment.author = request.user
-            subcomment.voteCount = 0
-            subcomment.comment_id = pk
-            subcomment.save()
-            return redirect(url)
-    #sonst CommentForm() nur anzeigens
-    else:
-        subcomment_form = SubcommentForm()
-    return render(request, 'Forum/commentDetail.html', {'post': post, 'comment': comment,'subcomments': subcomments, 'form': subcomment_form})
+#################################functions for subcomments##################################
 
+#update subcomments via pk and SubcommentForm
 def update_subcomment(request, pk):
     subcomment = Subcomment.objects.get(pk=pk)
     comment = subcomment.comment_id
@@ -315,6 +351,7 @@ def update_subcomment(request, pk):
     
     return render(request, 'Forum/updateComment.html', {'form': form})
 
+#delete subcomment via pk
 def delete_subcomment(request, pk):
     subcomment = Subcomment.objects.get(pk=pk)
     comment = subcomment.comment_id
@@ -323,13 +360,16 @@ def delete_subcomment(request, pk):
     subcomment.delete()
     return redirect(url)
 
+#report subcomment via pk
 def report_subcomment(request, pk):
+    #get the subcomment via pk=pk
     entry = Subcomment.objects.get(pk=pk)
     comment = entry.comment_id
     url = '/comment/' + str(comment) + '/detail/'
     if request.method == "POST":
         form = ReportSubcommentForm(request.POST)
         if form.is_valid():
+            #save the subcomment and its report comment into ReportSubcommentForm
             report = form.save(commit=False)
             report.reporter = request.user
             report.report_date = timezone.now()
@@ -341,8 +381,12 @@ def report_subcomment(request, pk):
         form = ReportSubcommentForm()
     return render(request, 'Forum/reportPost.html', {'form': form, 'entry': entry})
 
+###############################functions for adminpage#########################
 
+#release the report, input param pk an type(post, comment, subcomment)
 def release_report(request, pk, type):
+    #get the right object depending to typ and pk
+    #delete it only from the report table
     if type == 'comment':
         comment = Commentreport.objects.filter(comment_id = pk)
         comment.delete()
@@ -354,7 +398,10 @@ def release_report(request, pk, type):
         subcomment.delete()
     return redirect('reports')
 
+#delete the report (post,comment or subcomment)
 def delete_report(request,pk,type):
+    #get the right object depending to typ and pk
+    #delete it (linked Object in report table will automatically be deleted because of on_delete)
     if type == 'comment':
         comment = Comment.objects.get(pk = pk)
         comment.delete()
@@ -366,10 +413,13 @@ def delete_report(request,pk,type):
         subcomment.delete()
     return redirect('reports')
 
+#function for showing departments + adding 
 def add_department(request):
+    #only available with is_staff rights
     if request.user.is_staff == False:
         return HttpResponse('Unauthorized', status=401)
     departments = Department.objects.all()
+    #adding new department via Post-Request and Departmentform
     if request.method == "POST":
         form = DepartmentForm(request.POST)
         if form.is_valid():
@@ -380,29 +430,34 @@ def add_department(request):
         form = DepartmentForm()
     return render(request, 'Forum/departments.html', {'form': form, 'departments': departments})
 
+#delete the department via pk
 def delete_department(request, pk):
     department = Department.objects.get(pk=pk)
     department.delete()
     return redirect('departments')
 
+#set is_staff for user with pk = true
 def set_permission(request, pk):
     user = User.objects.get(pk = pk)
     user.is_staff = True
     user.save()
     return redirect('permissions')
 
+#set is_staff for user with pk = true
 def remove_permission(request, pk):
     user = User.objects.get(pk = pk)   
     user.is_staff = False
     user.save()
     return redirect('permissions')
 
-#Functions for registration, login and Logout of users
+#####################################Functions for registration,update, login and Logout of users#####################
+
+#register user via User Form  and EmployeeForm (fk=user.pk )
 def register_user(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         employee_form = EmployeeForm(request.POST)
-        #print(form.is_valid())
+
         if form.is_valid() and employee_form.is_valid():
             user = form.save()
 
@@ -419,13 +474,17 @@ def register_user(request):
         employee_form = EmployeeForm()
     return render(request, 'Forum/register.html', {'register_form': form, 'employee_form': employee_form})
 
+#update the user
 def update_user(request,pk):
     user = User.objects.get(pk=pk)
+
+    #check if employee with fk = user.pk exists
     try:
         employee = Employee.objects.get(user_id=pk)
     except:
         employee = None
     
+    #if employee exists -> use data from user + employee to update
     if employee != None:
         form = UserForm(request.POST or None, instance=user)
         employee_form = EmployeeForm(request.POST or None, instance=employee)
@@ -437,7 +496,7 @@ def update_user(request,pk):
             messages.success(request, "Profil erfolgreich bearbeitet")
             return redirect('overview')
 
-
+    #if employee doesnt exists -> use data from user and empty employee form to update
     else:
         form = UserForm(request.POST or None, instance=user)
         employee_form = EmployeeForm(request.POST)
@@ -456,6 +515,7 @@ def update_user(request,pk):
     
     return render(request, 'Forum/updateUser.html', {'form': form, 'employee_form': employee_form})
 
+#login via AuthenticationForm
 def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data = request.POST)
@@ -463,7 +523,7 @@ def login_user(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username = username, password=password)
-            #print("user: " + str(user))
+            
             if user is not None:
                 login(request, user)
                 messages.info(request, f"Servus und Willkommen: {username}!")
@@ -476,6 +536,7 @@ def login_user(request):
         form = AuthenticationForm()
     return render(request, 'Forum/login.html', {'login_form': form})
 
+#logout user via django-logout function
 def logout_user(request):
     logout(request)
     messages.info(request, "Auf Wiedersehen!")
